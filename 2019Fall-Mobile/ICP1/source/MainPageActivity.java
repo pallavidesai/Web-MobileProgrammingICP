9b1c189b5694c7db9a525c1213ef5bc9d45be88d
pallavidesai <s.pallavidesai@gmail.com>
2019-10-19 14:54:17 -0500

    icp

---
+++ 2019Fall-Mobile/ICP1/source/MainPageActivity.java
@@ -0,0 +1,40 @@
+package com.csee5590.helloworldapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.view.View;
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+    }
+
+    public void logOut(View v)
+    {
+        Intent redirect = new Intent(MainPageActivity.this, MainActivity.class);
+        startActivity(redirect);
+    }
+
+}
